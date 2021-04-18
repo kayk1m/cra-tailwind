b2cd368308ab2494a2654a9fea844d41c8507863
Kay Kim <kimjh@bawi.org>
2021-04-18 17:47:46 +0900

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+};
